5d1ca9a6ce50084a5130c9773003f4d49425e594
sairahul1526 <dravid.rahul1526@gmail.com>
2016-02-09 09:58:19 +0530

    Initial Commit

---
+++ PAYables/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PAYables
+//
+//  Created by Sai Krishna on 09/02/16.
+//  Copyright © 2016 Sai Krishna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
